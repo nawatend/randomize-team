29830420df27fb91d135821fb7e05673758e2027
nawatend <32384185+nawatend@users.noreply.github.com>
2020-05-21 16:30:58 +0200

    fix header

--- src/layouts/TopBar.jsx
+++ src/layouts/TopBar.jsx
@@ -4,8 +4,8 @@ import { Table, Button, TagInput, Heading } from 'evergreen-ui'
 export default function TopBar() {
   return (
     <div className="top__bar">
-      <Heading size={800}>Random Team Mates Generator</Heading>
-      <Heading size={400}>By LT. Future</Heading>
+      <Heading className="header" color="white" size={800}>Random Team Mates Generator</Heading>
+      <Heading className="header text__right" color="white" size={400}>LT. FUTURE</Heading>
     </div>
   )
 }
